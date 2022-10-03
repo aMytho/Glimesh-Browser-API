8cc5adc55575192c923244b5dd80a9c55d6cf630
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-10-02 21:04:50 -0500

    Remove default ts files

--- test/Num.test.ts
+++ test/Num.test.ts
@@ -1,13 +1,38 @@
-import { Num } from '../src/index'
+//import { Num } from '../src/index'
+
+class Num {
+    value: number
+
+    constructor(n: number) {
+        this.value = n
+    }
+
+    val(): number {
+        return this.value
+    }
+
+    add(n2: Num): Num {
+        this.value += n2.val()
+        return this
+    }
+
+    toString(): string {
+        return this.val().toString()
+    }
+
+    static addAll(numArr: Array<Num>): Num {
+        return new Num(numArr.map((n) => n.val()).reduce((a, b) => a + b, 0))
+    }
+}
 
 test('add', () => {
-  expect(new Num(5).add(new Num(6)).val()).toBe(11)
+    expect(new Num(5).add(new Num(6)).val()).toBe(11)
 })
 
 test('toString', () => {
-  expect(new Num(5).toString()).toBe('5')
+    expect(new Num(5).toString()).toBe('5')
 })
 
 test('addAll', () => {
-  expect(Num.addAll([new Num(5), new Num(2), new Num(13)]).val()).toBe(20)
+    expect(Num.addAll([new Num(5), new Num(2), new Num(13)]).val()).toBe(20)
 })
